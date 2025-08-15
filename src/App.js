20bc65e1eb4a70abd468990c2ae995edc5c3aa36
luisantonioj <luisantoniojopia@gmail.com>
2025-08-15 23:03:54 +0800

    rename home to about me

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ function App() {
               </button>
               <div className="collapse navbar-collapse" id="navbarNav">
                   <ul className="navbar-nav ms-auto">
-                      <li className="nav-item"><a className="nav-link" href="#home">Home</a></li>
+                      <li className="nav-item"><a className="nav-link" href="#home">About Me</a></li>
                       <li className="nav-item"><a className="nav-link" href="#portfolio">Portfolio</a></li>
                       <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
                   </ul>
